1fb51f434da6dabb126f33d1e5b5193a83891ef2
Peter Wilhelmsson <2hdddg@gmail.com>
2019-08-03 19:18:09 +0200

    Cleaned up and removed unused code

--- main.go
+++ main.go
@@ -9,43 +9,16 @@ import (
 	"github.com/2hdddg/mqtt/server"
 )
 
-func connection(conn net.Conn) {
-	//cont := true
-	r := &controlpacket.Reader{bufio.NewReader(conn)}
-	w := &controlpacket.Writer{conn}
+func makeSession(conn net.Conn) {
+	rd := &controlpacket.Reader{bufio.NewReader(conn)}
+	wr := &controlpacket.Writer{conn}
 
-	sess, err := server.Connect(conn, r, w)
+	sess, err := server.Connect(conn, rd, wr)
 	if err != nil {
-		fmt.Println("Connection refused:", err)
 		return
 	}
 
-	fmt.Println("Connected")
 	sess.Start()
-
-	/*
-		handler := &controlpacket.Handler{
-			OnConnect: func(c *controlpacket.Connect) {
-				// Protocol error if CONNECT is sent again!
-				cont = false
-			},
-			OnDisconnect: func(d *controlpacket.Disconnect) {
-				cont = false
-			},
-			OnPublish: func(p *controlpacket.Publish) {
-				fmt.Println(p)
-			},
-		}
-
-		for cont {
-			err := r.ReadPacket(4, handler)
-			if err != nil {
-				fmt.Println(err)
-				cont = false
-			}
-		}
-		conn.Close()
-	*/
 }
 
 func main() {
@@ -63,7 +36,7 @@ func main() {
 			fmt.Println("Failed to accept")
 			return
 		}
-		go connection(conn)
+		makeSession(conn)
 	}
 }
 
