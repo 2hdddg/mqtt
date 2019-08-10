143d792f2e91cd3fd6f92232344d86b6999b068f
Peter Wilhelmsson <2hdddg@gmail.com>
2019-08-10 19:16:56 +0200

    More subscription tests

--- server/subscription.go
+++ server/subscription.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 
 	"github.com/2hdddg/mqtt/packet"
 	"github.com/2hdddg/mqtt/topic"
@@ -38,7 +37,6 @@ func (s *subscriptions) subscribe(sub *packet.Subscription) packet.QoS {
 func (s *subscriptions) match(t *topic.Name) (bool, packet.QoS) {
 	qoS := -1
 	for _, sub := range s.subs {
-		fmt.Println("Evaluating subscription", sub)
 		if sub.filter.Match(t) {
 			if int(sub.qoS) > qoS {
 				qoS = int(sub.qoS)
--- server/subscription_test.go
+++ server/subscription_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/2hdddg/mqtt/packet"
+	"github.com/2hdddg/mqtt/topic"
 )
 
 func TestSubscribe(t *testing.T) {
@@ -33,7 +34,71 @@ func TestSubscribe(t *testing.T) {
 		s := newSubscriptions()
 		qoS := s.subscribe(&c.sub)
 		if qoS != c.qoS {
-			t.Errorf("Expected %v but got %v", c.qoS, qoS)
+			t.Errorf("Expected QoS %v but got %v", c.qoS, qoS)
+		}
+	}
+}
+
+func TestMatch(t *testing.T) {
+	testcases := []struct {
+		subs    []packet.Subscription
+		topic   *topic.Name
+		qoS     packet.QoS
+		matched bool
+	}{
+		// One match
+		{
+			[]packet.Subscription{
+				packet.Subscription{
+					QoS:   packet.QoS0,
+					Topic: "x/y",
+				},
+			},
+			topic.NewName("x/y"),
+			packet.QoS0,
+			true,
+		},
+		// No match
+		{
+			[]packet.Subscription{
+				packet.Subscription{
+					QoS:   packet.QoS0,
+					Topic: "x/y",
+				},
+			},
+			topic.NewName("x/z"),
+			packet.QoSFailure,
+			false,
+		},
+		// Two matches, pick highest QoS
+		{
+			[]packet.Subscription{
+				packet.Subscription{
+					QoS:   packet.QoS0,
+					Topic: "x/y",
+				},
+				packet.Subscription{
+					QoS:   packet.QoS2,
+					Topic: "x/#",
+				},
+			},
+			topic.NewName("x/y"),
+			packet.QoS2, // Should get highest!
+			true,
+		},
+	}
+	for _, c := range testcases {
+		s := newSubscriptions()
+		for _, sub := range c.subs {
+			s.subscribe(&sub)
+		}
+		matched, qoS := s.match(c.topic)
+		if matched != c.matched {
+			t.Errorf("Expected matched %v but was %v",
+				c.matched, matched)
+		}
+		if qoS != c.qoS {
+			t.Errorf("Expected QoS %v but got %v", c.qoS, qoS)
 		}
 	}
 }
