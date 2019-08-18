61b6eddfc4c2e3772a819ca185859d16d27072c0
Peter Wilhelmsson <2hdddg@gmail.com>
2019-08-18 09:47:52 +0200

    Prototyping client subscribe

--- client/session.go
+++ client/session.go
@@ -7,21 +7,24 @@ import (
 	"github.com/2hdddg/mqtt/conn"
 	"github.com/2hdddg/mqtt/logger"
 	"github.com/2hdddg/mqtt/packet"
+	"github.com/2hdddg/mqtt/topic"
 	"github.com/2hdddg/mqtt/writequeue"
 )
 
 type Session struct {
-	ClientId  string
-	keepAlive time.Duration
-	conn      conn.C
-	stopChan  chan bool
-	log       logger.L
-	wrQueue   *writequeue.Queue
+	ClientId   string
+	keepAlive  time.Duration
+	conn       conn.C
+	stopChan   chan bool
+	log        logger.L
+	wrQueue    *writequeue.Queue
+	subscrChan chan *subscriptions
 }
 
 func (s *Session) received(px packet.Packet) {
 	switch p := px.(type) {
 	case *packet.PingResp:
+	case *packet.SubscribeAck:
 
 	default:
 		s.log.Error(fmt.Sprintf("Received unhandled packet %t", p))
@@ -63,6 +66,23 @@ func (s *Session) pump() {
 			readAsync()
 			//}
 
+		// Received subscribe request, forward to server
+		case sub := <-s.subscrChan:
+			subs := []packet.Subscription{}
+			for _, x := range sub.subs {
+				subs = append(subs, packet.Subscription{
+					Topic: x.Filter.String(),
+					QoS:   x.QoS,
+				})
+			}
+			p := &packet.Subscribe{
+				PacketId:      666,
+				Subscriptions: subs,
+			}
+			s.wrQueue.Add(&writequeue.Item{
+				Packet: p,
+			})
+
 		// Read failure
 		case err := <-readErrChan:
 			s.log.Error(fmt.Sprintf("Receive error %s", err))
@@ -83,14 +103,36 @@ func (s *Session) Dispose() {
 	<-s.stopChan
 }
 
-func NewSession(conn conn.C, log logger.L, keepAliveSecs uint16) *Session {
+type Subscription struct {
+	Filter *topic.Filter
+	QoS    packet.QoS
+}
+
+type SubscribeAck func(subs []Subscription)
+
+type subscriptions struct {
+	subs []Subscription
+	ack  SubscribeAck
+}
+
+func (s *Session) Subscribe(subs []Subscription, ack SubscribeAck) {
+	s.subscrChan <- &subscriptions{
+		subs: subs,
+		ack:  ack,
+	}
+}
+
+func NewSession(
+	conn conn.C, log logger.L, keepAliveSecs uint16) *Session {
+
 	s := &Session{
-		ClientId:  "x",
-		conn:      conn,
-		keepAlive: time.Duration(keepAliveSecs) * time.Second,
-		log:       log,
-		stopChan:  make(chan bool),
-		wrQueue:   writequeue.New(conn, log),
+		ClientId:   "x",
+		conn:       conn,
+		keepAlive:  time.Duration(keepAliveSecs) * time.Second,
+		log:        log,
+		stopChan:   make(chan bool),
+		wrQueue:    writequeue.New(conn, log),
+		subscrChan: make(chan *subscriptions),
 	}
 
 	go s.pump()
--- example/mqttclnt/main.go
+++ example/mqttclnt/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"os"
 
 	"github.com/2hdddg/mqtt/client"
 	"github.com/2hdddg/mqtt/conn"
 	"github.com/2hdddg/mqtt/logger"
+	"github.com/2hdddg/mqtt/packet"
+	t "github.com/2hdddg/mqtt/topic"
+
 )
 
-func main() {
+func connect() *client.Session {
 	nc, err := net.Dial("tcp", "localhost:6666")
 	if err != nil {
 		fmt.Println("Failed to connect")
-		return
+		return nil
 	}
-	defer nc.Close()
+	//defer nc.Close()
 	conn := conn.New(nc)
 
 	opts := client.Options{
@@ -28,10 +33,56 @@ func main() {
 	err = client.Connect(conn, &opts, log)
 	if err != nil {
 		fmt.Println("MQTT server did not allow", err)
+		return nil
+	}
+	return client.NewSession(conn, log, opts.KeepAliveSecs)
+}
+
+func main() {
+	if len(os.Args) <= 1 {
 		return
 	}
 
-	_ = client.NewSession(conn, log, opts.KeepAliveSecs)
+	var topic string
+	var qos int
+
+	flags := flag.NewFlagSet("x", flag.ExitOnError)
+	cmd := os.Args[1]
+	switch cmd {
+	case "subscribe":
+		flags.StringVar(&topic, "topic", "", "Topic filter")
+		flags.IntVar(&qos, "qos", 0, "Quality of service")
+	default:
+		fmt.Println("Unknown command:", cmd)
+		return
+	}
+	flags.Parse(os.Args[2:])
+	//fmt.Println(flags.Args())
+
+	switch cmd {
+	case "subscribe":
+		if topic == "" {
+			fmt.Println("Missing topic")
+			return
+		}
+	}
+
+	sess := connect()
+	if sess == nil {
+		return
+	}
+
+	switch cmd {
+	case "subscribe":
+		sess.Subscribe([]client.Subscription{
+			client.Subscription{
+				Filter: t.NewFilter(topic),
+				QoS:    packet.QoS(qos),
+			}},
+			func(acked []client.Subscription) {
+			})
+	}
+
 	for {
 	}
 }
